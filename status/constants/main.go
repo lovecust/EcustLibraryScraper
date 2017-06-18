0f42f61974e384f2b75bbe2bc1156a3d25ece9a4
Fisher <fisher.subs@gmail.com>
2017-06-18 22:23:20 +0800

    Added status/constants.

---
+++ status/constants/main.go
@@ -0,0 +1,10 @@
+package constants
+
+// Ecust Library(Fengxian) status address.
+var URL_LIBRARY_STATUS = "http://lib.ecust.edu.cn:8081/gateseat/lrp.aspx"
+
+// FLOORS_COUNT is the floors count of Ecust Library, Fengxian District.
+var FLOORS_COUNT = 6
+
+// SEATS_OF_EACH_FLOOR stands the seats amount of each floor.
+var SEATS_OF_EACH_FLOOR = []int64{36, 322, 461, 372, 349, 56}
